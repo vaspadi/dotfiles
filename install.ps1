5e7d9ede131a5b473096621b9edabcbed0d5e4d9
vaspadi <vasiliev.work1@gmail.com>
2026-01-22 22:49:29 +0200

    feat(lazygit): add delta

--- install.ps1
+++ install.ps1
@@ -1,1 +1,8 @@
 New-Item -ItemType SymbolicLink -Path $HOME\.wezterm.lua -Target $HOME\dotfiles\.wezterm.lua
+
+if ($IsWindows) {
+  New-Item -ItemType SymbolicLink -Path "$env:APPDATA/lazygit/config.yml" -Target "$HOME/dotfiles/lazygit/config-windows.yml"
+} elseif ($IsLinux) {
+  New-Item -ItemType SymbolicLink -Path "$HOME/.config/lazygit/config.yml" -Target "$HOME/dotfiles/lazygit/config-linux.yml"
+}
+
