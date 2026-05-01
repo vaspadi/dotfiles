0ac9bbfeea719ddb6b5468cca4455b3fda064ec2
vaspadi <vasiliev.work1@gmail.com>
2026-05-01 14:59:54 +0300

    fix(nvim): lsp of vue

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,6 +4,25 @@ return {
     inlay_hints = {
       enabled = false,
     },
+    servers = {
+      vtsls = false,
+      tsgo = {},
+      vtsls = {
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        tsserver = {
+          globalPlugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vim.fn.stdpath("data")
+                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+              languages = { "vue" },
+              configNamespace = "typescript",
+            },
+          },
+        },
+      },
+      vue_ls = {},
+    },
   },
   keys = {
     {
